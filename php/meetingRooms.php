d6513ff28ce6c310e1abacd09e90cdad3c0a0e52
Jake King <jkinghgp17@gmail.com>
2017-03-29 00:50:14 +0100

    Added folders for javascript and html. Comments added to meetingRooms.php

--- php/meetingRooms.php
+++ php/meetingRooms.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * Jake King April 2017
+ * Meeting Room Signout Page EHGP
+ * Allows signing out of meeting rooms
+ * Editing already signed out rooms
+ * Shows what time are taken on any given day
+ */ 
 session_start();
 include 'functions_2.php';
 //teacher_only();
@@ -8,14 +15,18 @@ include 'includeInc_2.php';
 dohtml_header("Schedule Meeting Rooms");
 echo "<table class ='centered'><tr><td>";
 homeLogout();
+// Was Getting issues in firefox when this was not here
+// May or may not still be needed
 date_default_timezone_set('America/New_York');
 
+// Set the $date variable
 if (isset($_GET['dateUpdate'])) {
 	$date=$_GET['dateUpdate'];
 } else {
 	$date = date('Y/m/d');
 }
 
+// Delete or edit 
 if (isset($_POST['time']) && isset($_POST['date']) && isset($_POST['room'])) {
 	$date = $_POST['date'];
 	if ($_POST['number'] == 0) {
@@ -60,6 +71,7 @@ echo "</td></tr></table>";
 	<input type="submit" value="Submit">
 </form>
 
+/* This form allows user to change the current date being shown in the table */
 <form action="meetingRooms.php" method="GET" align='center'>
 <input name="dateUpdate" type="date" value=<?php $date ?>>
 <br/><input type="submit" value="Change Date">
@@ -91,10 +103,16 @@ echo "</td></tr></table>";
 			$query->bindValue(':date', $date);
 			$query->execute();
 			$rows = $query->fetchAll();
+			/*
+			 * This loop goes though all hours at 30 min intervals 
+			 * Sets up table that will be drawn by javascript code at 
+			 * a later point
+			 */ 
 			$hour = 7;
 			$min = 30;
 			for($row = 0; $row < 14 * 60; $row++) {
 				echo "<tr>";
+				// Sets the $time string 
 				if ($min == 0) {
 					echo "<td>" . $hour . ":0" . $min . "</td>";
 				} else if ($min == 30) {
@@ -126,6 +144,7 @@ echo "</td></tr></table>";
 						if ($rooms[$col] == $r['room']) {
 							if ($time >= $r['starttime'] && $time <= $r['endtime'])
 								$color = 'Red';
+							// A form is added to the start of every time group to allow for editing
 							if ($time == $r['starttime']) {
 								$str = $r['teacher'] . " " . $r['number'] . " " . "<br/><form action=meetingRooms.php method=POST><input type=hidden name=room value=" . $rooms[$col] . "><input type=hidden name=date value=" . $date . "><input type=hidden name=time value=" . $time . "><input type=number name=number min=0 max=999><input type=submit></form>";
 							}
